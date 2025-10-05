eb0e114bc564f3ace3b97b2dda400b9c253b2b98
Aloysiuskw <aloysiuskw95@gmail.com>
2025-10-05 14:44:04 +0800

    bug fix store.tsx

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -7,13 +7,13 @@ export default function Store() { return (
     <section className="p-6 space-y-6">
         <h1 className="text-3xl font-bold">Featured</h1>
         <FeaturedCarousel items={featuredData} index={0} onChange={() => {}} />
-            
-    </section>
-  <div className="p-6 rounded-xl bg-[#1F2833] text-[#F8F9FA]">
-   {/* Game Carousel Section */}
-      <div>
+      <div className="p-6 rounded-xl bg-[#1F2833] text-[#F8F9FA]">
+     {/* Game Carousel Section */}
+        <div>
         <GameCarousel />
-      </div>
-</div>
+        </div>
+     </div>    
+    </section>
+  
 );
 }
